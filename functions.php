ac0ed7f32783b8338320bf841081a50cb51e96b4
Mike Sinkula <mike@mikesinkula.com>
2017-04-25 18:30:48 -0700

    Creating a WordPress Theme From a Template

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Theme Name: Web170 Sping 2017 Section 01 Demo Theme
+Theme URI: https://github.com/msinkula/web170-sp17-02
+Author: Mike Sinkula
+Author URI: http://mikesinkula.com/
+Description: This is the demo theme for my Web170 section 01 classs in the Spring 2017 Quarter. 
+Version: 42.0
+*/
+
+?>
