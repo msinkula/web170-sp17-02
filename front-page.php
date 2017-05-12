6b1d819b74c18532fcb4de189c463f57f8b55e3a
Mike Sinkula <mike@mikesinkula.com>
2017-05-11 18:23:05 -0700

    Create a Static Home Page with Multiple Loops for Your Site

--- front-page.php
+++ front-page.php
@@ -14,15 +14,26 @@
     <div id="widgets">
         <section class="widget-item">
             <h2>About Me:</h2>
-            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet.</p>
+            <?php if(have_posts()) : while(have_posts()) : the_post(); // loop one ?>
+            <?php the_content(); ?>
+            <?php endwhile; endif; ?>
         </section>
         <section class="widget-item">
-            <h2>Latest Postings:</h2>
+            <h2>Latest News:</h2>
             <ul>
-            <li><a href="#">Lorem ipsum dolor</a></li>
-            <li><a href="#">Aliquam tincidunt</a></li>
-            <li><a href="#">Vestibulum auctor</a></li>
-            <li><a href="#">Pellentesque habitant</a></li>
+            <?php rewind_posts(); // stop loop one ?>
+            <?php query_posts(array('category_name' => 'news', 'posts_per_page' => 4 )); // give directions to loop two ?>
+            <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+            <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><br><small>Posted on <?php the_time('F j, Y'); ?> by <?php the_author(); ?> in <?php the_category(); ?></small></li>
+            <?php endwhile; endif; ?>
+            </ul>
+            <h2>Latest Articles:</h2>
+            <ul>
+            <?php rewind_posts(); // stop loop two ?>
+            <?php query_posts(array('category_name' => 'articles', 'posts_per_page' => 4)); // give directions to loop three ?>
+            <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+            <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><br><small>Posted on <?php the_time('F j, Y'); ?> by <?php the_author(); ?> in <?php the_category(); ?></small></li>
+            <?php endwhile; endif; ?>
             </ul>
         </section>
         <section class="widget-item">
