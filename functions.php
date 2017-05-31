98228a0b3cd9841ab2ad437f888b22ed2f2ac32a
Mike Sinkula <mike@mikesinkula.com>
2017-05-31 14:40:30 -0700

    Search Engine Optimization (SEO) in WordPress without Using a Plugin

--- functions.php
+++ functions.php
@@ -25,4 +25,40 @@ register_nav_menus(array('main-menu' => __('Main Menu'), 'footer-menu' => __('Fo
 add_theme_support('post-thumbnails');
 //
 
+// Add Post Type Support
+add_post_type_support('page', 'excerpt');
+//
+
+function get_mikes_title_tag() {
+	
+	global $post; // DO NOT FORGET THIS IF USING A POST OBJECT
+	
+	if (is_front_page() || is_home()) { // front page or blog feed
+
+		bloginfo('description');
+	
+	} elseif (is_page() || is_single()) { // page or posting
+		
+		echo get_the_title($post->ID);
+		
+	} else { // 404 or search, etc.
+		
+		bloginfo('description');
+		
+	}
+	
+	if ( $post->post_parent ) { // for your site’s Parent Pages
+    
+        echo ' | '; // separator with spaces
+        echo get_the_title($post->post_parent); // retrieve the parent page title
+        
+	}
+	
+	echo ' | ';
+	echo 'Business Name';
+	echo ' | ';
+	echo 'Location';
+	
+}
+
 ?>
